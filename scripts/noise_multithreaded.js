fd12cf820fa9f181e3ae77e94ffb150556680966
Steven Sell <ssell@ocularinteractive.com>
2017-02-22 00:34:35 -0500

    Worker thread termination

--- scripts/noise_multithreaded.js
+++ scripts/noise_multithreaded.js
@@ -22,24 +22,46 @@
  */
 
 /**
+ * Destroys all workers in the provided array.
+ * \param[in] workers Array of workers.
+ */
+function destroyWorkers(workers) {
+    for(var i = 0; i < workers.length; ++i) {
+        workers[i].terminate();
+        workers[i] = null;
+    }
+}
+
+/**
+ * Triggers the workers responsible for non-normalized noise generation.
  * 
+ * \param[in] surface        Target surface to receive the generated noise image.
+ * \param[in] image          Temporary image to place noise within.
+ * \param[in] noise          String name of the noise algorithm to employ.
+ * \param[in] params         Parameter string used in noise construction.
+ * \param[in] numWorkersSide Number of workers to spawn per surface dimension.
+ * \param[in] widthStep      Width, in pixels, that each worker is responsible for.
+ * \param[in] heightStep     Height, in pixels, that each worker is responsible for.
+ * \param[in] endCallback    Callback function used to signal the end of noise generation.
  */
 function triggerNoiseWorkers(surface, image, noise, params, numWorkersSide, widthStep, heightStep, endCallback) {
 
     var numWorkersComplete = 0;
+    var workers = Array(numWorkersSide * numWorkersSide);
 
     for(var x = 0; x < numWorkersSide; ++x) {
         for(var y = 0; y < numWorkersSide; ++y) {
 
-            var worker = new Worker("scripts/noise.js");
+            var index  = (x * numWorkersSide) + y;
             var startX = (x * widthStep);
             var endX   = ((x + 1) * widthStep);
             var startY = (y * heightStep);
             var endY   = ((y + 1) * heightStep);
 
-            worker.postMessage({isRaw: false, image: image, noise: noise, noiseParams: params, width: image.width, height: image.height, startX: startX, endX: endX, startY: startY, endY: endY});
+            workers[index] = new Worker("scripts/noise.js");
+            workers[index].postMessage({isRaw: false, image: image, noise: noise, noiseParams: params, width: image.width, height: image.height, startX: startX, endX: endX, startY: startY, endY: endY});
 
-            worker.onmessage = function(e) {  
+            workers[index].onmessage = function(e) {  
                 if(e.data.progress) { 
                     NoiseProgressBar.update(e.data.progress);
                 } else {
@@ -47,6 +69,7 @@ function triggerNoiseWorkers(surface, image, noise, params, numWorkersSide, widt
                     numWorkersComplete++;
 
                     if(numWorkersComplete == (numWorkersSide * numWorkersSide)) {
+                        destroyWorkers(workers);
                         surface.updateRawImageData();
                         endCallback();
                     }
@@ -57,7 +80,7 @@ function triggerNoiseWorkers(surface, image, noise, params, numWorkersSide, widt
 }
 
 /**
- * 
+ * Places the raw data from sourceRaw into the destRaw container.
  */
 function putRawData(sourceRaw, destRaw, width, startX, endX, startY, endY) {
     var index = 0;
@@ -71,7 +94,16 @@ function putRawData(sourceRaw, destRaw, width, startX, endX, startY, endY) {
 }
 
 /**
+ * Triggers the workers responsible for normalized noise generation.
  * 
+ * \param[in] surface        Target surface to receive the generated noise image.
+ * \param[in] image          Temporary image to place noise within.
+ * \param[in] noise          String name of the noise algorithm to employ.
+ * \param[in] params         Parameter string used in noise construction.
+ * \param[in] numWorkersSide Number of workers to spawn per surface dimension.
+ * \param[in] widthStep      Width, in pixels, that each worker is responsible for.
+ * \param[in] heightStep     Height, in pixels, that each worker is responsible for.
+ * \param[in] endCallback    Callback function used to signal the end of noise generation.
  */
 function triggerNormalizedWorkers(surface, image, noise, params, numWorkersSide, width, height, widthStep, heightStep, endCallback) {
 
@@ -83,18 +115,22 @@ function triggerNormalizedWorkers(surface, image, noise, params, numWorkersSide,
     var min = Infinity;
     var max = -Infinity;
 
+    var numWorkers = (numWorkersSide * numWorkersSide);
+    var workers = Array(numWorkers);
+
     for(var x = 0; x < numWorkersSide; ++x) {
         for(var y = 0; y < numWorkersSide; ++y) {
 
-            var worker = new Worker("scripts/noise.js");
+            var index  = (x * numWorkersSide) + y;
             var startX = (x * widthStep);
             var endX   = ((x + 1) * widthStep);
             var startY = (y * heightStep);
             var endY   = ((y + 1) * heightStep);
 
-            worker.postMessage({ isRaw: true, rawData: rawData, noise: noise, noiseParams: params, width: width, height: height, startX: startX, endX: endX, startY: startY, endY: endY});
+            workers[index] = new Worker("scripts/noise.js");
+            workers[index].postMessage({ isRaw: true, rawData: rawData, noise: noise, noiseParams: params, width: width, height: height, startX: startX, endX: endX, startY: startY, endY: endY});
 
-            worker.onmessage = function(e) {
+            workers[index].onmessage = function(e) {
                 if(e.data.progress) {
                     NoiseProgressBar.update(e.data.progress);
                 } else {
@@ -102,8 +138,10 @@ function triggerNormalizedWorkers(surface, image, noise, params, numWorkersSide,
                     putRawData(e.data.rawData, rawData, width, e.data.startX, e.data.endX, e.data.startY, e.data.endY);
                     numWorkersComplete++;
 
-                    if(numWorkersComplete == (numWorkersSide * numWorkersSide)) {
+                    if(numWorkersComplete == numWorkers) {
 
+                        // Raw noise generation is complete. Time to normalize the data in a separate worker.
+                        
                         NoiseProgressBar.setTitle("Normalizing Data ...");
 
                         var normWorker = new Worker("scripts/noise_normalization.js");
@@ -113,6 +151,9 @@ function triggerNormalizedWorkers(surface, image, noise, params, numWorkersSide,
                         normWorker.onmessage = function(e) {
                             surface.drawImage(e.data.image);
                             surface.updateRawImageData();
+                            
+                            destroyWorkers(workers);
+                            destroyWorkers([normWorker]);
 
                             endCallback();
                         };
@@ -167,24 +208,29 @@ function applyPaletteMultithreaded(surface, numWorkersSide, endCallback) {
 
     const numWorkers = (numWorkersSide * numWorkersSide);
 
+    var workers = Array(numWorkers);
+
     for(var x = 0; x < numWorkersSide; ++x) {
         for(var y = 0; y < numWorkersSide; ++y) {
-            var worker = new Worker("scripts/surface.js");
+            var index  = (x * numWorkersSide) + y;
             var startX = (x * widthStep);
             var endX   = ((x + 1) * widthStep);
             var startY = (y * heightStep);
             var endY   = ((y + 1) * heightStep);
 
-            worker.postMessage({ srcImage: srcImage, destImage: destImage, paletteDescr: paletteDescr, startX: startX, endX: endX, startY: startY, endY: endY });
+            workers[index] = new Worker("scripts/surface.js");
+            workers[index].postMessage({ srcImage: srcImage, destImage: destImage, paletteDescr: paletteDescr, startX: startX, endX: endX, startY: startY, endY: endY });
         
-            worker.onmessage = function(e) {
+            workers[index].onmessage = function(e) {
                 if(e.data.progress) {
                     NoiseProgressBar.update(e.data.progress);
                 } else {
                     surface.drawImageRect(e.data.image, e.data.startX, e.data.endX, e.data.startY, e.data.endY);
+
                     numWorkersComplete++;
 
                     if(numWorkersComplete == numWorkers) {
+                        destroyWorkers(workers);
                         endCallback();
                     }
                 }
